500d2edf15f4e8218de900f9862017b86f9e5c98
Rasheedah aka Rocky <rjhpenndrive@gmail.com>
2020-12-03 19:42:46 -0500

    This is the financial.js file holding our database columns

---
+++ models/financial.js
@@ -0,0 +1,29 @@
+//Only setting up columns inside of the table
+
+module.exports = function(sequelize, DataTypes) {
+    var FinancialInfo = sequelize.define("FinancialInfo", {
+     
+        goal: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+        creditScore: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      currentDebt: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      currentRetirement: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      advice: {
+        type: DataTypes.STRING
+      }
+    });
+    return FinancialInfo;
+  };
+ 
