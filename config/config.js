4fd49f4077adc89a5acf7f94d91c389fc257b5f3
viktor vaughn <dereck8187@gmail.com>
2020-12-10 20:33:49 -0500

    got the front end fully connected to backend

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "1Madmin1",
+    "password": "Ballin87",
     "database": "financialapp",
     "host": "127.0.0.1",
     "dialect": "mysql"
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
       // Giving the Author model a name of type STRING
-      name: DataTypes.STRING
+      name: DataTypes.STRING,
+      financeGoal: DataTypes.STRING,
+      creditScore: DataTypes.STRING,
+      currentDebt: DataTypes.STRING,
+      retirement: DataTypes.BOOLEAN,
+
     });
   
     
