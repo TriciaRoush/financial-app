0c8f0fd4469b54928a8ca02ca4bcd6352d1ccd52
viktor vaughn <dereck8187@gmail.com>
2020-12-12 12:51:08 -0500

    final changes to finished results screen

--- public/js/results.js
+++ public/js/results.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
     console.log(userid)
 $.get("/api/user?id="+userid).then((user)=>{
 console.log(user)
+$("#username").text(user[0].fname)
+$("#goal").text(user[0].financeGoal)
+$("#creditScore").text(user[0].creditScore)
+$("#debt").text(user[0].currentDebt)
+$("#retire").text(user[0].retirement)
 })
 })
